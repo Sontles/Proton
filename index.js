5db38d765a5ae8f73305aa5ffbc47bafab9ee7c0
Sontles <kwepadoodle@gmail.com>
2018-09-29 16:00:16 +0200

    JNGERGRWEQ

--- index.js
+++ index.js
@@ -27,19 +27,19 @@ client.on("guildMemberAdd", (member) => {
    console.log(`${membertag} left`);
  });
  client.on("message", (message) => {
-    var YUGOAPI = message.content.toLowerCase();
+    var PROTONAPI = message.content.toLowerCase();
 if(message.content.startsWith('whats a proton?') || message.content.startsWith('whats a proton?')) {
     message.author.send('wow, read this you dumb fuck')
     message.author.send('https://en.wikipedia.org/wiki/Proton')
  }
-    if(YUGOAPI == "i dont like protons") {
+    if(PROTONAPI == "i dont like protons") {
     message.channel.send(`wow, you are so fucking mean`)
     }
     
-        if(YUGOAPI == "fbi") {
+        if(PROTONAPI == "fbi") {
         message.channel.send(`https://twitter.com/WindyC0ck/status/1031790769345052672`)
         }
-    if(YUGOAPI == "yeet") {
+    if(PROTONAPI == "yeet") {
         message.channel.sendFile ("https://cdn.discordapp.com/attachments/463351454590042115/482238072113856522/Yeet_Sound_Effect.mp3", "yeet.mp3")
         }
 })

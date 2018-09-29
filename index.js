5eb34633d59fd97cee005b80a26314fcd2b10b9d
Sontles <kwepadoodle@gmail.com>
2018-09-29 14:59:56 +0200

    big update yay... ps i copied yugos code for this since josia fucked something up again

--- index.js
+++ index.js
@@ -26,20 +26,28 @@ client.on("guildMemberAdd", (member) => {
 "general").send(`${membertag} left the server! What a meanie...`);
    console.log(`${membertag} left`);
  });
-var mRespond = {
-    "ayy": "Ayy lmao!",
-    "who made proton?": "Proton was made by <@260415308169347083>"
-};
-
-function getResponse(message) {mRespond[message.content.toLowerCase()];
-}
-
-client.on('message', (message) => {
-    var response =
-    getResponse(message);
-    if (response) {
-
-        message.channel.sendMessage(response);
+ client.on("message", (message) => {
+    var YUGOAPI = message.content.toLowerCase();
+if(message.content.startsWith('whats a proton?') || message.content.startsWith('whats a proton?')) {
+    message.author.send('wow, read this you dumb fuck')
+    message.author.send('https://en.wikipedia.org/wiki/Proton')
+ }
+    if(YUGOAPI == "i dont like protons") {
+    message.channel.send(`wow, you are so fucking mean`)
     }
-}); 
+    
+        if(YUGOAPI == "fbi") {
+        message.channel.send(`https://twitter.com/WindyC0ck/status/1031790769345052672`)
+        }
+    if(YUGOAPI == "yeet") {
+        message.channel.sendFile ("https://cdn.discordapp.com/attachments/463351454590042115/482238072113856522/Yeet_Sound_Effect.mp3", "yeet.mp3")
+        }
+})
+
 client.login(config.token)
+
+ client.on('ready', () => {
+    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+    client.user.setStatus('Online')
+    client.user.setActivity('Playing with Reality')
+});
